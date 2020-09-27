12ca9eba41219574e9620cf94d25c6e61bc1339c
unknown <kmcdermott31@gmail.com>
2020-09-27 22:16:35 +0100

    working on favourites from LS

--- src/containers/MyCakesContainer.js
+++ src/containers/MyCakesContainer.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MyCakesContainer = () => {
-    return ( 
-        <div>
+class MyCakesContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            favCakes: []
+         }
+    }
+
+    componentDidMount() {
+        const favourtieCakes = [];
+        Object.keys(localStorage).forEach(function(key){
+            favourtieCakes.push(localStorage.getItem(key))
+        })
+        this.setState({
+            favCakes: favourtieCakes
+        })
+    }
+
+    render() { 
+        return ( 
+            <div>
                 <h2>My Favourite Cakes</h2>
+
         </div>
-     );
+         );
+    }
 }
  
 export default MyCakesContainer;
